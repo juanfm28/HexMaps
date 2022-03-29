194df1f58dcc32825db6a96bb6c6b32d72e7345f
Juan Fonseca <fonseca.juan8@gmail.com>
2022-03-29 14:58:56 -0600

    Small map editor implemented
    Small map editor as in the tutorial
    
    Will branch from here

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class HexGrid : MonoBehaviour
 {
     public int width = 6;
     public int height = 6;
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
 
     public HexCell cellPrefab;
     public Text cellLabelPrefab;
@@ -44,10 +48,21 @@ public class HexGrid : MonoBehaviour
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
 
         Text label = Instantiate<Text>(cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
         label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
         label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
     }
 }
